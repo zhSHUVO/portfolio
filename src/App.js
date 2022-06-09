e25cfba5595165a5ee933acb6f6b88e7a44e830e
zhSHUVO <zhshuvo99@gmail.com>
2022-06-09 09:28:17 +0600

    app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Navbar from "./Pages/Navbar/Navbar";
+import Porjects from "./Pages/Projects/Porjects";
+import Skills from "./Pages/Skills/Skills";
+import Testimonials from "./Pages/Testimonials/Testimonials";
 
 function App() {
     return (
         <div className="App">
-            <h1>Home</h1>
+            <Navbar></Navbar>
+            <Routes>
+                <Route path="/" element={<Home></Home>}></Route>
+            </Routes>
+            <Routes>
+                <Route path="/home" element={<Home></Home>}></Route>
+            </Routes>
+            <Routes>
+                <Route path="/about" element={<About></About>}></Route>
+            </Routes>
+            <Routes>
+                <Route path="/projects" element={<Porjects></Porjects>}></Route>
+            </Routes>
+            <Routes>
+                <Route path="/skills" element={<Skills></Skills>}></Route>
+            </Routes>
+            <Routes>
+                <Route
+                    path="/testimonials"
+                    element={<Testimonials></Testimonials>}
+                ></Route>
+            </Routes>
+            <Routes>
+                <Route path="/contact" element={<Contact></Contact>}></Route>
+            </Routes>
         </div>
     );
 }
